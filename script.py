1cf0980837a62b97430a1aa522133ee8f5babbff
rebeccabartels <beccacode@gmail.com>
2020-05-03 13:09:17 -0400

    sunday

--- script.py
+++ script.py
@@ -1,6 +1,18 @@
 import os
 import sys
 
+
+def collect_user_information():
+    # Prompt the user for their username
+    filter1 = input("What do you wish to filter? ")
+    filter2 = input("What do you wish to filter? ")
+    filter3 = input("What do you wish to filter? ")
+    # The list that will store all of the user's data inside of it
+    user_info = [filter1, filter2, filter3]
+
+    # Return a list with username, email, and password
+    return user_info
+count = 0
 for root, subdirs, files in os.walk("/home/riley/CY0420"):
     #print(root)
     #print(subdirs)
@@ -10,7 +22,7 @@ for root, subdirs, files in os.walk("/home/riley/CY0420"):
         if os.path.isdir(filePath):
             f = open("/home/riley/Dev/CyberSecTA/Apps/Python Data Filterer/filter.txt", "a")
             f.write(str(filePath) + "\n")
-            print(filePath)
+            count += 1
         else:
-            print(filter)
+            print(count)
 f.close()
