ff94e8b0a220d71eedc59949c23723ed877979d9
rebeccabartels <beccacode@gmail.com>
2020-05-03 14:44:36 -0400

    made tuples

--- script.py
+++ script.py
@@ -1,28 +1,26 @@
 import os
 import sys
+count = 0 
+my_list = list()
 
-
-def collect_user_information():
-    # Prompt the user for their username
-    filter1 = input("What do you wish to filter? ")
-    filter2 = input("What do you wish to filter? ")
-    filter3 = input("What do you wish to filter? ")
-    # The list that will store all of the user's data inside of it
-    user_info = [filter1, filter2, filter3]
-
-    # Return a list with username, email, and password
-    return user_info
-count = 0
-for root, subdirs, files in os.walk("/home/riley/CY0420"):
+def iterate():
+    for root, subdirs, files in os.walk("/home/riley/CY0420"):
     #print(root)
     #print(subdirs)
     #print(files)
-    for file in os.listdir(root):
-        filePath = root + '/' + file
-        if os.path.isdir(filePath):
-            f = open("/home/riley/Dev/CyberSecTA/Apps/Python Data Filterer/filter.txt", "a")
-            f.write(str(filePath) + "\n")
-            count += 1
-        else:
-            print(count)
-f.close()
+        for file in os.listdir(root):
+            filePath = root + '/' + file
+            if os.path.isdir(filePath):
+                count = +1
+                f = open("/home/riley/Dev/CyberSecTA/Apps/Python Data Filterer/filter.txt", "a")
+                f.write(str(filePath) + "\n")
+                my_list.insert(count, filePath)              
+    f.close()
+    return my_list
+
+iterate()
+
+
+
+counter_list = list(enumerate(my_list, 1))
+print(counter_list)
