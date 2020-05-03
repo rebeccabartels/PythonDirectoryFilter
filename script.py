9ee8080460452c20451ee1e3f3803c896294ac60
rebeccabartels <beccacode@gmail.com>
2020-05-02 20:56:52 -0400

    curating

--- script.py
+++ script.py
@@ -7,7 +7,9 @@ folder_path = os.path.join("/home/riley/CY0420", fileName)
 #for root, dirs, files in os.walk(folder_path): 
 #    print("All Work and No Play Makes Jack A Dull Boy")
 All_Paths = list()
-Patters = ["/solved", "/Solved", "/SOLVED", ""]
+Patterns = list()
+#Pattern = input("Please enter a filter string: ")
+
 
 
 for root, dirs, files in os.walk("/home/riley/CY0420"):
@@ -20,17 +22,18 @@ for root, dirs, files in os.walk("/home/riley/CY0420"):
         #current_week = open(current_file_path, "r")
         #print("SUCCESS")
         if file_name in os.listdir("/home/riley/CY0420"):  #set target as current file path so that we can add it / manipulate it 
-            target_dir = current_file_path
+            #target_dir = file_name
             #print("TARGET: " + str(current_file_path)) 
             #add the current target / current file path to the All_Paths array for storage 
-            All_Paths.append(current_file_path) 
+            #All_Paths.append(file_name) 
              #Dev/CyberSecTA/Apps/Python\ Data\ Filterer/
             f = open("/home/riley/Dev/CyberSecTA/Apps/Python Data Filterer/filter.txt", "a")
             f.write(str(current_file_path) + "\n")
-        print(f)
-        f.close()
-            #target_dir = os.path.join(root, target_dir_name)
-#print("This All the Paths:" + " " + str(All_Paths))
+            print(f)
+            
+    f.close()
+    #print("ALL PATHS: " + str(All_Paths))
+
 
             
 
@@ -38,15 +41,7 @@ for root, dirs, files in os.walk("/home/riley/CY0420"):
 #Must clear the array after every program run 
 #print("NOT EMPTY: " + str(All_Paths))
 All_Paths = []
-#print("EMPTY: " + str(All_Paths))
-
-
-
-
-
-
-#find ./CY0420  -iwholename */solved* > excludefiles
-        
+print("EMPTY: " + str(All_Paths))
         
 #keywords to filter out: solution, solved, student guide, lesson plan, .gitattributes, units 4- the rest 
 
