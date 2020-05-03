5c83d102804ba655fafed711d67fa7e2f71e3060
rebeccabartels <beccacode@gmail.com>
2020-05-02 23:12:51 -0400

    new algorithm

--- script.py
+++ script.py
@@ -1,18 +1,16 @@
 import os
 import sys
 
-fileName = "" 
-folder_path = os.path.join("/home/riley/CY0420", fileName)
-
-for root, dirs, files in os.walk("/home/riley/CY0420"):
-    for file_name in files:
-        current_file_path = os.path.join(root, file_name)
-        if file_name in os.listdir("/home/riley/CY0420"):   #set target as current file path so that we can add it / manipulate it 
+for root, subdirs, files in os.walk("/home/riley/CY0420"):
+    #print(root)
+    #print(subdirs)
+    #print(files)
+    for file in os.listdir(root):
+        filePath = root + '/' + file
+        if os.path.isdir(filePath):
             f = open("/home/riley/Dev/CyberSecTA/Apps/Python Data Filterer/filter.txt", "a")
-            f.write(str(current_file_path) + "\n")
-            print(f)
-    file_name = open(current_file_path, "r")
-    f.close()
-    file_name.close()
-        
-#keywords to filter out: solution, solved, student guide, lesson plan, .gitattributes, units 4- the rest 
+            f.write(str(filePath) + "\n")
+            print(filePath)
+        else:
+            print(filter)
+f.close()
